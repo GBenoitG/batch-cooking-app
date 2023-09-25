adf0f3aa5f71f5fb92022ca44bb3055b53b48675
GBenoitG <contact@benoitguillot.fr>
2023-09-25 03:18:27 +0200

    Clean up code base

--- domain/src/main/java/fr/bendev/batchcookingapp/domain/model/Sample.kt
+++
@@ -1,9 +0,0 @@
-package fr.bendev.batchcookingapp.domain.model
-/**
- *
- * DESCRIPTION: Pure Kotlin data class reflecting each business object used in app
- * FILE NAMING CONVENTION: {Object}
-*/
- data class Sample(
-    val param: String
- )

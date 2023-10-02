91b19c7f56a08be16c4780abdf86249f1f91fbbd
GBenoitG <contact@benoitguillot.fr>
2023-10-02 14:47:15 +0200

    Create Batch database

---
+++ framework/src/main/java/fr/bendev/batchcookingapp/framework/database/BatchDatabase.kt
@@ -0,0 +1,44 @@
+package fr.bendev.batchcookingapp.framework.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import fr.bendev.batchcookingapp.framework.utils.Constants
+
+@Database(
+    entities = [],
+    version = 1,
+    exportSchema = false
+)
+abstract class BatchDatabase : RoomDatabase() {
+
+    /**
+     * DAOs
+     */
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: BatchDatabase? = null
+
+        fun getDatabase(context: Context): BatchDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    BatchDatabase::class.java,
+                    Constants.DB_NAME
+                )
+                    .addMigrations(*BatchDatabaseMigration.migrations)
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
